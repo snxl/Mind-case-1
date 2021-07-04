16650f7dbe5c762ecc67643b58ae93e5741553c7
Nicolas <nicolas.soares.gomes@gmail.com>
2021-07-04 20:53:50 -0300

    rotas completas - validações

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 import userController from "../controller/userController.js"
 import middlewareValidator from "../middlewares/validateDatas.js"
 import middlewareAuthenticator from "../middlewares/validateAuthorization.js"
+import middlewareAuthorizationAdministrator from '../middlewares/middlewareAuthorizationAdministrator.js';
 
 
 const router = express.Router();
 
-router.get('/', userController.indexUser);
+router.get('/allusers', middlewareAuthorizationAdministrator, userController.allUserAdm);
 
 router.post('/signup', middlewareValidator.singUp , userController.store);
 router.post("/singin", middlewareValidator.singIn, userController.indexUser)
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -67,6 +67,8 @@ export default new class UserService{
 
     async indexAllUser(){
 
+        return await db.User.findAll()
+
     }
 
     async update(token, data){
